f99b0daf4f5a3832bda5498e153ac4bc143fd316
computational-biology <cs.parthajit.roy@gmail.com>
2021-06-27 17:30:51 +0530

    helix.h added

---
+++ src/include/helix.h
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  helix.h
+ *
+ *    Description:  This header file manipulates hlx file. 
+ *
+ *        Version:  1.0
+ *        Created:  27/06/21 01:34:03 PM IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  PARTHAJIT ROY (PR), roy.parthajit@gmail.com
+ *   Organization:  The University of Burdwan
+ *
+ * =====================================================================================
+ */
+
+
+
+#ifndef  __helix_H__
+#define  __helix_H__
+
+#include <stdlib>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
+
+
+
+#endif   /* ----- #ifndef __helix_H__  ----- */
